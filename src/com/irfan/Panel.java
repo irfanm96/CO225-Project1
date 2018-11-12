60538f3d55f826fc6dad4f0d5cfdbb18ac651cd4
irfanm96 <e15138@ce.pdn.ac.lk>
2018-11-12 16:55:10 +0530

    moved printcomponent to main

--- src/com/irfan/Panel.java
+++ src/com/irfan/Panel.java
@@ -27,13 +27,6 @@ public  class Panel extends JPanel { // inherit JPanel
 		// call paintComponent from parent class
 		super.paintComponent(g);
 
-		for(int i=0 ; i<this.width+1 ; i++) {
-			for(int j =0 ; j<this.height+1;j++) {
-				Point point=new Point(i,j); // create points
-				Complex z=point.findMapping(this,-1,1,-1,1); // map to complex plane
-				 printPoint((Graphics2D) g, z.test1(1000), point);
-			}
-		}
 	}
 
 	public static void main(String [] args) {
@@ -42,8 +35,20 @@ public  class Panel extends JPanel { // inherit JPanel
 		JFrame frame = new JFrame("Serpenski's Triangle");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+		int x=600;
+		int y=800;
 		// set the content of the frame as one of this panel
-		frame.setContentPane(new Panel(600, 800));
+		Panel p=new Panel(x, y);
+		frame.setContentPane(p);
+
+		for(int i=0 ; i<x+1 ; i++) {
+			for(int j =0 ; j<y+1;j++) {
+				Point point=new Point(i,j); // create points
+				Complex z=point.findMapping(p,-1,1,-1,1); // map to complex plane
+				printPoint(frame, z.test1(1000), point);
+			}
+		}
 
 		frame.pack();
 		frame.setLocationRelativeTo(null);
