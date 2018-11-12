cb69d0e8ccc62ffef8b8ab12691a21cb93470b82
irfanm96 <e15138@ce.pdn.ac.lk>
2018-11-12 21:52:22 +0530

    added thread with input validation

--- src/com/irfan/Fractals.java
+++ src/com/irfan/Fractals.java
@@ -14,6 +14,8 @@ public class Fractals extends Thread {
     private static int iterations;
     private static String whichTest;
     private static Panel p;
+    private static int width = 600;
+    private static int height = 800;
 
 
     public Fractals(int start, int end) {
@@ -33,22 +35,31 @@ public class Fractals extends Thread {
         return p;
     }
 
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
 
     public void run() {
         if (whichTest.equals("J")) {
             printJuliaSet(iterations);
-        } else {
+            return;
+        }if (whichTest.equals("M")){
             printMandelBrotSet(x1, x2, y1, y2, iterations);
+
         }
 
     }
 
     public void printMandelBrotSet(double x1, double x2, double y1, double y2, int iterations) {
 
-        for (int i = 0; i < getP().getWidth() + 1; i++) {
+        for (int i = 0; i < getWidth() + 1; i++) {
             for (int j = getStart(); j < getEnd() + 1; j++) {
                 Point point = new Point(i, j); // create points
-                Complex z = point.findMapping(getP(), x1, x2, y1, y2); // map to complex plane
+                Complex z = point.findMapping(getP(), -1, 1, -1, 1); // map to complex plane
                 Panel.printPoint((Graphics2D) getP().getGraphics(), z.mandelBrotSetTest(iterations), point);
 
             }
@@ -59,10 +70,10 @@ public class Fractals extends Thread {
 
     public void printJuliaSet(int iterations) {
 
-        for (int i = 0; i < getP().getWidth() + 1; i++) {
+        for (int i = 0; i < getWidth() + 1; i++) {
             for (int j = getStart(); j < getEnd() + 1; j++) {
                 Point point = new Point(i, j); // create points
-                Complex z = point.findMapping(p, -1, 1, -1, 1); // map to complex plane
+                Complex z = point.findMapping(getP(), -1, 1, -1, 1); // map to complex plane
                 Panel.printPoint((Graphics2D) getP().getGraphics(), z.juliaSetTest(iterations), point);
             }
         }
@@ -81,7 +92,7 @@ public class Fractals extends Thread {
     public static void main(String[] args) {
 
         String setName;
-        if (args.length==0) {
+        if (args.length == 0) {
             System.out.println("Please enter other options");
             return;
         }
@@ -93,24 +104,28 @@ public class Fractals extends Thread {
             setName = "JuliaSet";
             whichTest = "J";
 
-            for (int i = 1; i < 3; i++) {
+            for (int i = 1; i < 4; i++) {
                 if (!isNumeric(args[i])) {
                     System.out.println("Invalid Input,need double/int for range and int for iterations for julia set");
                     return;
                 }
             }
-            Fractals.iterations=Integer.parseInt(args[3]);
-            int y = getP().getHeight() / 4;
-            createThread(setName, y, getP().getHeight());
-            return;
+            Fractals.iterations = Integer.parseInt(args[3]);
+            int y = getHeight() / 4;
+            Fractals.createThread(setName, y);
+
 
         }
+
+
         if (args.length == 6) {
-            if (!args[0].toLowerCase().equals("mendelbrot")) {
+            if (!args[0].toLowerCase().equals("mandelbrot")) {
                 System.out.println("Invalid Input,Please enter write keyword mandelbrot/julia");
                 return;
             }
             setName = "MandelBrotSet";
+            whichTest = "M";
+
 
             for (int i = 1; i < 6; i++) {
                 if (!isNumeric(args[i])) {
@@ -119,34 +134,35 @@ public class Fractals extends Thread {
                 }
             }
 
-            Fractals.iterations=Integer.parseInt(args[5]);
+            Fractals.iterations = Integer.parseInt(args[5]);
 
-            Fractals.x1=Double.parseDouble(args[1]);
+            Fractals.x1 = Double.parseDouble(args[1]);
 
-            Fractals.x2=Double.parseDouble(args[2]);
+            Fractals.x2 = Double.parseDouble(args[2]);
 
-            Fractals.y1=Double.parseDouble(args[3]);
+            Fractals.y1 = Double.parseDouble(args[3]);
 
-            Fractals.y2=Double.parseDouble(args[4]);
+            Fractals.y2 = Double.parseDouble(args[4]);
 
-            int y = getP().getHeight() / 4;
-            createThread(setName, y, getP().getHeight());
+            int y = getHeight() / 4;
+            System.out.println(y);
+            Fractals.createThread(setName, y);
 
         }
-
-
     }
 
-    private static void createThread(String name, int y, int height) {
-        Fractals.p = Panel.createPanel(800, 800, name);
-        Fractals t1 = new Fractals(0, y);
-        Fractals t2 = new Fractals(y, 2 * y);
-        Fractals t3 = new Fractals(2 * y, 3 * y);
-        Fractals t4 = new Fractals(3 * y, height);
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
 
+        private static void createThread (String name,int y){
+            Fractals.p = Panel.createPanel(getWidth(), getHeight(), name);
+            Fractals t1 = new Fractals(0, y);
+            Fractals t2 = new Fractals(y, 2 * y);
+            Fractals t3 = new Fractals(2 * y, 3 * y);
+            Fractals t4 = new Fractals(3 * y, getHeight());
+            t1.start();
+            t2.start();
+            t3.start();
+            t4.start();
+
+        }
     }
-}
+
